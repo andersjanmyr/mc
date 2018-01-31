ed9b734f6919de70c195ecb68367febdfdc5d489
Anders Janmyr <anders@janmyr.com>
2018-01-30 19:55:58 -0800

    Support reading value from file

--- cmd/set.go
+++ cmd/set.go
@@ -2,6 +2,7 @@ package cmd
 
 import (
 	"fmt"
+	"io/ioutil"
 	"os"
 
 	"github.com/andersjanmyr/mc/pkg/mc"
@@ -10,6 +11,7 @@ import (
 )
 
 var expiration int32
+var file string
 
 var setCmd = &cobra.Command{
 	Use:   "set",
@@ -17,21 +19,39 @@ var setCmd = &cobra.Command{
 	Long:  `Sets a key and value in memcached`,
 	Args:  cobra.MinimumNArgs(1),
 	Run: func(cmd *cobra.Command, args []string) {
+		key := args[0]
+		filename, err := cmd.Flags().GetString("file")
+		if err != nil {
+			fmt.Fprintln(os.Stderr, err)
+			os.Exit(1)
+		}
+		var value []byte
+		if len(args) == 2 {
+			value = []byte(args[1])
+		} else if filename != "" {
+			value, err = ioutil.ReadFile(filename)
+			if err != nil {
+				fmt.Fprintln(os.Stderr, err)
+				os.Exit(1)
+			}
+		} else {
+			fmt.Fprintln(os.Stderr, "Value or filename (-f) are required")
+			os.Exit(1)
+		}
+
+		exp, err := cmd.Flags().GetInt32("expiration")
+		if err != nil {
+			fmt.Fprintln(os.Stderr, err)
+			os.Exit(1)
+		}
 		memcached, err := mc.Connect()
 		if err != nil {
 			fmt.Fprintln(os.Stderr, err)
 			os.Exit(1)
 		}
-
-		exp, err := cmd.Flags().GetInt32("expiration")
-		fmt.Println(exp)
-		if err != nil {
-			fmt.Fprintln(os.Stderr, err)
-			os.Exit(1)
-		}
 		err = memcached.Set(&memcache.Item{
-			Key:        args[0],
-			Value:      []byte(args[1]),
+			Key:        key,
+			Value:      value,
 			Expiration: exp,
 		})
 		if err != nil {
@@ -43,5 +63,6 @@ var setCmd = &cobra.Command{
 
 func init() {
 	setCmd.Flags().Int32VarP(&expiration, "expiration", "e", 0, "Expiration time for this key")
+	setCmd.Flags().StringVarP(&file, "file", "f", "", "Filename containing the value")
 	rootCmd.AddCommand(setCmd)
 }
